36b1b38fc3b6377d035c9d8c6790b2f90dea4294
SemyonDenisov <semdenis2003@mail.ru>
2025-06-19 23:00:29 +0400

    initial commit

---
+++ yandex/src/test/java/ru/practicum/yandex/YandexApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.yandex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
