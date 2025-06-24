22e400de86359222689f2ee6c74f448af4bfc383
SemyonDenisov <semdenis2003@mail.ru>
2025-06-24 20:16:37 +0400

    change app schema and change cartService logic

--- yandex/src/main/java/ru/practicum/yandex/service/cartService/CartService.java
+++ yandex/src/main/java/ru/practicum/yandex/service/cartService/CartService.java
@@ -1,9 +1,7 @@
 package ru.practicum.yandex.service.cartService;
 
 import ru.practicum.yandex.model.Cart;
-import ru.practicum.yandex.model.Item;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CartService {
