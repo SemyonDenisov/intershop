a9d9fe13578ee6b369756e946c98fa4d6d8245d9
SemyonDenisov <semdenis2003@mail.ru>
2025-07-11 23:58:33 +0400

    make react integration tests for services

--- yandex/src/main/java/ru/practicum/yandex/model/Cart.java
+++ yandex/src/main/java/ru/practicum/yandex/model/Cart.java
@@ -26,11 +26,13 @@ public class Cart {
     Set<Item> items;
 
     public Cart() {
-        id=1;
     }
 
     public double getTotal() {
         AtomicReference<Double> total = new AtomicReference<>(0.0);
+        if(items == null) {
+            return 0.0;
+        }
         this.getItems()
                 .forEach(item -> total.updateAndGet(v -> v + item.getPrice() * item.getCount()));
         return total.get();
--- yandex/src/test/java/ru/practicum/yandex/integration/BaseIntegrationTests.java
+++ yandex/src/test/java/ru/practicum/yandex/integration/BaseIntegrationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.yandex.DAO.CartRepository;
 import ru.practicum.yandex.DAO.ItemsRepository;
 import ru.practicum.yandex.DAO.OrderItemRepository;
@@ -20,11 +21,13 @@ public class BaseIntegrationTests {
     OrderRepository orderRepository;
     @Autowired
     OrderItemRepository orderItemRepository;
+
+    @Transactional
     @BeforeEach
     public void baseSetUp() {
-        cartRepository.deleteAll();
-        orderRepository.deleteAll();
-        orderItemRepository.deleteAll();
-        itemsRepository.deleteAll();
+        cartRepository.deleteAll().block();
+        orderRepository.deleteAll().block();
+        orderItemRepository.deleteAll().block();
+        itemsRepository.deleteAll().block();
     }
 }
